63066f25b789d85a88e7fdffeef2b179fb66786c
Virginie Delort <virginie.delort@laposte.net>
2025-06-09 09:49:45 +0200

    Ajout du script SQL et mise à jour du modèle EER

--- src/main/java/com/openclassrooms/PayMyBuddy/PayMyBuddyApplication.java
+++ src/main/java/com/openclassrooms/PayMyBuddy/PayMyBuddyApplication.java
@@ -1,7 +1,9 @@
 package com.openclassrooms.PayMyBuddy;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class PayMyBuddyApplication {
@@ -9,5 +11,6 @@ public class PayMyBuddyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
 	}
+	
 
 }
